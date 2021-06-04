b6bad4b1173cf457c8c862117c51fa6b9a78e978
mao.kirameki <mao.kirameki@techacademy.jp>
2021-06-04 07:08:37 +0000

    課題2.1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,10 @@
         @include('commons.navbar')
 
         <div class="container">
+                    
+            {{-- エラーメッセージ --}}
+            @include('commons.error_messages')
+            
             @yield('content')
         </div>
 
