e07a3d4ebd11f278fa449bd71722d1c1eb4b6af0
mao.kirameki <mao.kirameki@techacademy.jp>
2021-06-04 02:12:08 +0000

    CRUD tasks

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,22 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// デフォルトのコメント部分は省略
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// CRUD
+// メッセージの個別詳細ページ表示
+Route::get('tasks/{id}', 'TasksController@show');
+// メッセージの新規登録を処理（新規登録画面を表示するためのものではありません）
+Route::post('tasks', 'TasksController@store');
+// メッセージの更新処理（編集画面を表示するためのものではありません）
+Route::put('tasks/{id}', 'TasksController@update');
+// メッセージを削除
+Route::delete('tasks/{id}', 'TasksController@destroy');
+
+// index: showの補助ページ
+Route::get('tasks', 'TasksController@index');
+
+Route::resource('tasks', 'TasksController');
